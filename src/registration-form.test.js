77365bf6ede3336d9926763ab1a0f9da1cda5f18
ignaspangonis <ignas.pangonis@mif.stud.vu.lt>
2024-01-28 21:37:43 +0200

    Remove redundant test

--- src/registration-form.test.js
+++ src/registration-form.test.js
@@ -34,39 +34,3 @@ describe('suninjuly.github.io/registration1.html', () => {
     )
   })
 })
-
-describe('suninjuly.github.io/registration2.html', () => {
-  let driver
-  const TEST_URL = 'http://suninjuly.github.io/registration2.html'
-
-  beforeEach(async () => {
-    driver = await new Builder().forBrowser('chrome').build()
-    await driver.get(TEST_URL)
-  })
-
-  afterEach(async () => await driver.quit())
-
-  // This test should fail
-  it.skip('fills and submits the form', async () => {
-    const firstNameInput = await driver.findElement(By.className('first'))
-    const lastNameInput = await driver.findElement(By.css('.first_block .second'))
-    const emailInput = await driver.findElement(By.className('third'))
-    const submitButton = await driver.findElement(By.className('btn'))
-
-    assert.ok(await firstNameInput.isDisplayed())
-    // Below assertion fails, because the last name input is not displayed
-    assert.ok(await lastNameInput.isDisplayed())
-    assert.ok(await emailInput.isDisplayed())
-    assert.ok(await submitButton.isDisplayed())
-
-    await firstNameInput.sendKeys('First name')
-    await lastNameInput.sendKeys('Last name')
-    await emailInput.sendKeys('email')
-    await submitButton.click()
-
-    assert.equal(
-      'Congratulations! You have successfully registered!',
-      await driver.findElement(By.css('h1')).getText(),
-    )
-  })
-})
